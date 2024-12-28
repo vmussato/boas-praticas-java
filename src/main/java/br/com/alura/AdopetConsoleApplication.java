4d0ee9d839a155f88f4e7cb16edea4422f260cad
Vinícius Mussato <vinicius.mussato@gmail.com>
2024-12-28 12:16:23 -0300

    Isolando código do Httpclient

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import br.com.alura.client.ClientHttpConfiguration;
 import br.com.alura.service.AbrigoService;
 import br.com.alura.service.PetService;
 import com.google.gson.JsonArray;
@@ -20,8 +21,10 @@ public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
 
-        AbrigoService abrigoService = new AbrigoService();
-        PetService petService = new PetService();
+        ClientHttpConfiguration client = new ClientHttpConfiguration();
+
+        AbrigoService abrigoService = new AbrigoService(client);
+        PetService petService = new PetService(client);
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
         try {
             int opcaoEscolhida = 0;
